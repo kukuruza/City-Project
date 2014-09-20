8c66991c79ff1dba0799ff5c46592b7280de39a4
Evgeny Toropov <etoropov@andrew.cmu.edu>
2014-09-20 12:11:37 -0400

    cleans pipeline.m, adds a script to reformat image names

--- src/pipeline/pipeline.m
+++ src/pipeline/pipeline.m
@@ -1,20 +1,24 @@
-% pipeline
+% test pipeline
+%
+% This file reads images from a directory and processes them one by one,
+%   as if they were coming real-time.
 %
 
-im0 = imread('somewhere');
+imDir = '/Users/evg/Box Sync/City Project/data/five camera for 2 min/cameraNumber360/';
+imNames = dir ([imDir, 'image*.jpg']);
+
+im0 = imread([imDir, imNames(1).name]);
 
 backSubtractor = BackgroundSubtractor(im0);
 
 geom = GeometryEstimator(im0);
 
 
-
-i = 0;
+i = 2;
 while 1
     
-    name = 'get some name(i)';
-    
-    im = imread(name);
+    % read image
+    im = imread([imDir, imNames(i).name]);
     gray = rgb2gray(im);
     
     % subtract backgroubd and return mask
@@ -24,17 +28,17 @@ while 1
     
     % geometry should process the mask
     [ROIs, scales, orientation] = geom.guess(foregroundMask);
-    assert(size(ROIs,1) == 4);
-    assert(isvector(scale));
-    %assert(orientation makes sense)
-    assert(size(ROIs,2) == length(scales) && size(ROIs,2) == length(orientations));
+    
+    assert (isempty(ROIs) || size(ROIs,1) == 4);
+    assert (isempty(scale) || isvector(scale));
+    assert (size(ROIs,2) == length(scales) && size(ROIs,2) == length(orientations));
     N = size(ROIs,2);
     
     % actually detect cars
     cars = cell(1,N);
-    for iPatch = 1 : N
-        roi = ROIs(j);
-        patch;% = gray %%%
+    for j = 1 : N
+        roi = ROIs(j,:);
+        patch = gray (roi(2) : roi(4), roi(1) : roi(3));
         cars(j) =  detectCar(patch, scales(j), orientations(j));
     end
     
@@ -42,4 +46,5 @@ while 1
     
     % counting
     
+    i = i + 1;
 end
