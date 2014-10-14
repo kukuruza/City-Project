48331801fde1bbb35f93726d61e878c883ba268a
Evgeny Toropov <etoropov@andrew.cmu.edu>
2014-10-14 10:00:35 -0400

    add interface for GeometryEstimator for convinience

---
+++ src/geometry/GeometryInterface.m
@@ -0,0 +1,15 @@
+% Interface for GeometryEstimator
+%   Its goal is to 1) provide function definitions to users of the class
+%   2) provide definitions of feature-request functions
+
+classdef GeometryInterface
+    methods (Abstract)
+        
+        % get map for sizes of cars on the road (0 for forbiden areas)
+        getCameraRoadmask (obj, inputImg)
+        
+        % get probability for a car to move from A to B
+        getMutualProb (obj, car1, car2, frameDiff)
+
+    end % methods
+end
