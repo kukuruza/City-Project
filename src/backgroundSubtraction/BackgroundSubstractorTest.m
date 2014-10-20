5ad5a8d972ef5fe5dd960de798b799fcfee638fb
LynnaGui <guily05@gmail.com>
2014-10-19 23:50:56 -0400

    maskProcess function

--- src/backgroundSubtraction/BackgroundSubstractorTest.m
+++ src/backgroundSubtraction/BackgroundSubstractorTest.m
@@ -14,12 +14,16 @@ videoPath = [CITY_DATA_PATH '2-hours/camera572.avi'];
 %% test
 
 frameReader = FrameReaderVideo (videoPath);
-subtractor = BackgroundSubtractor (5, 30, 50);
+subtractor = BackgroundSubtractor (5, 25, 80);
 
 while true
     frame = frameReader.getNewFrame();
     [mask, bboxes] = subtractor.subtract(frame);
     frame_out = subtractor.drawboxes(frame, bboxes);
-    imshow(frame_out);
+    %imshow(frame_out);
+    maskOut = maskProcess(mask);
+  %   subplot(1,2,1),imshow(frame);
+  %   subplot(1,2,2),
+    imshow(maskOut);
     pause(0.5);
 end
