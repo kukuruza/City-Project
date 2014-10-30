ba72a450cb08be5fee337abb9c025a407acad4ee
satwikkottur <satwik27@gmail.com>
2014-10-30 18:09:35 -0400

    FrameReaderVideo now handles reading and returning timestamps

--- src/io/FrameReaderTest.m
+++ src/io/FrameReaderTest.m
@@ -12,23 +12,29 @@ cd (fileparts(mfilename('fullpath')));
 % setup data directory
 run '../rootPathsSetup.m';
 
+% Change the video path accordingly
+% Time stamp path is expected to be of the same name but with .txt
+% extension
+videoPath = [CITY_DATA_PATH, 'cam572/2-hours-morning/2-hours-morning.avi'];
+timeStampPath = strrep(videoPath, '.avi', '.txt');
+frameReader = FrameReaderVideo (videoPath, timeStampPath);
 
-videoPath = [CITY_DATA_PATH, '2-min/camera360.avi'];
-%frameReader = FrameReaderVideo (videoPath);
-
-imDir = [CITY_DATA_PATH, '2-min/camera360/'];
+%imDir = [CITY_DATA_PATH, '2-min/camera360/'];
 %frameReader = FrameReaderImages (imDir);
 
-frameReader = FrameReaderInternet (572);
+%frameReader = FrameReaderInternet (572);
 
 
 while true
     tic
     pause(0.1)
-    frame = frameReader.getNewFrame();
+    [frame, timeStamp] = frameReader.getNewFrame();
     if isempty(frame)
         break
     end
+    
+    % Time Display
+    %fprintf('Time (yy mm dd hh:mm:ss): %d %d %d %d %d %f)\n', timeStamp);
     
     imshow(frame)
     toc
--- src/io/FrameReaderVideo.m
+++ src/io/FrameReaderVideo.m
@@ -10,21 +10,27 @@ classdef FrameReaderVideo < FrameReader
         counter = 1;
     end % properties
     methods
-        function FR = FrameReaderVideo (videoPath, intervalsPath)
-            if ~exist(videoPath,'file') || ~exists(intervalsPath, 'file')
-                fprintf ('FrameReaderVideo: files: %s, %s\n', videoPath, intervalsPath);
-                error ('FrameReaderVideo: videoPath or intervalsPath doesn''t exist');
+        function FR = FrameReaderVideo (videoPath, timeStampPath)
+            if ~exist(videoPath,'file') || ~exist(timeStampPath, 'file')
+                fprintf ('FrameReaderVideo: files: %s, %s\n', videoPath, timeStampPath);
+                error ('FrameReaderVideo: videoPath or timeStampPath doesn''t exist');
             end
-            FR.timesTable = dlmread(intervalsPath);
+            FR.timesTable = dlmread(timeStampPath);
+            
+            % Checking if the timestamp is indeed 6-elemented
+            if(size(FR.timesTable, 2) ~= 6)
+                error('FrameReaderVideo:Timestamp', ...
+                    'TimeStamps expected to be 6 elemented : yyyy mm dd hh:mm:ss');
+            end
             FR.videoSource = vision.VideoFileReader(videoPath, ...
                 'ImageColorSpace','RGB','VideoOutputDataType','uint8'); 
         end
-        function [frame, timeinterval] = getNewFrame(FR)
+        function [frame, timeStamp] = getNewFrame(FR)
             [frame, EOF] = step(FR.videoSource);
             if EOF == true
                 frame = [];
             end
-            timeinterval = FR.timesTable(FR.counter);
+            timeStamp = FR.timesTable(FR.counter, :);
             FR.counter = FR.counter + 1;
         end
     end % methods
