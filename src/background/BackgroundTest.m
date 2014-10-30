fb1526abb3adcca97eacbacd4f8db75bbdb17ce8
LynnaGui <guily05@gmail.com>
2014-10-30 19:22:31 -0400

    save images from background test

--- src/background/BackgroundTest.m
+++ src/background/BackgroundTest.m
@@ -16,7 +16,7 @@ run ../subdirPathsSetup.m
 
 %% input and ground truth
 imagesDir = [CITY_DATA_PATH 'camdata/cam572/2-hours/'];
-
+outImagePath = [CITY_DATA_PATH 'testdata/background/result/'];
 
 
 %% test
@@ -36,7 +36,7 @@ background.fp_level = 1;
 % extracting bounding boxes
 background.minimum_blob_area = 50;
          
-
+N=0;
 while true
     frame = frameReader.getNewFrame();
     %[mask, bboxes] = subtractor.subtract(frame);
@@ -45,5 +45,8 @@ while true
     frame_out = background.drawboxes(frame, bboxes);
     subplot(1,2,1),imshow(frame_out);
     subplot(1,2,2),imshow(mask);
+    imname = sprintf('result%d.png',N);
+    imwrite(mask,fullfile(outImagePath,imname));
     pause(0.5);
+    N=N+1;
 end
