a2d2eb4c0fffacc51b0867844c4081446341c94f
satwikkottur <satwik27@gmail.com>
2015-05-01 18:46:47 -0400

    Candidates - additional functionality written, made method to be implemented

--- src/candidates/CandidatesInterface.m
+++ src/candidates/CandidatesInterface.m
@@ -2,12 +2,20 @@
 % Interface for choosing candidate boxes, e.g. selective search
 %
 
-
 classdef CandidatesInterface
     methods (Abstract)
-        
         % bboxes = [x1 y1 width height] x N
+        % Each row for a bounding box
+        % Generating the candidates
         bboxes = getCandidates (self, varargin)
         
+        % Visualizing the candidates
+        debugImg = drawCandidates (bboxes, image);
+        
+        % Save the candidate boxes
+        saveCandidates(bboxes, fileName);
+        
+        % Load the candidate boxes from the given file
+        bboxes = loadCandidates(fileName);
     end
 end
