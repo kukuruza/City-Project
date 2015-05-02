c175be60a28ebbb62137183089007a1691ec768a
Evgeny Toropov <etoropov@andrew.cmu.edu>
2015-05-01 22:01:48 -0400

    adds selective search for candidates

---
+++ src/candidates/CandidatesBase.m
@@ -0,0 +1,42 @@
+classdef CandidatesBase < CandidatesInterface
+    
+    methods(Static)
+        % Visualizing the candidates
+        function debugImg = drawCandidates (bboxes, image)
+            % get random colors
+            cmap = colormap(lines(size(bboxes,1)));
+            % init with random colors
+            shapeInserter = vision.ShapeInserter( ...
+                'BorderColor','Custom','CustomBorderColor',uint8(cmap * 255));
+            % draw
+            debugImg = step(shapeInserter, image, uint32(bboxes));
+        end
+        
+        % Save the candidate boxes
+        function saveCandidates(bboxes, fileName)
+            fileId = fopen(fileName, 'w');
+            
+            % Writing it to the file
+            for i = 1:size(bboxes, 1)
+                fprintf(fileId, '%d %d %d %d\n', ...
+                    bboxes(i, 1), bboxes(i, 2), bboxes(i, 3), bboxes(i, 4));
+            end
+            
+            fclose(fileId);
+        end
+        
+        % Load the candidate boxes from the given file
+        function bboxes = loadCandidates(fileName)
+            if ~exist(fileName, 'file')
+                warning('File name %s does not exist!\n', fileName);
+                bboxes = [];
+            else
+                % Reading from the file
+                fileId = fopen(fileName, 'r');
+                bboxes = textscan(fileId, '%d %d %d %d\n');
+                bboxes = cell2mat(bboxes);
+            end
+        end
+    end % methods(Static)
+
+end
--- src/candidates/CandidatesTest.m
+++ src/candidates/CandidatesTest.m
@@ -2,18 +2,33 @@
 
 % Setting up the paths
 run ../rootPathsSetup.m
-
-% Takes in the roadMap for a camera and generates the candidates
-cands = Candidates();
+run ../subdirPathsSetup.m
 
 % Reading the image and mask
 camId = 572;
 image = imread(fullfile(CITY_DATA_PATH, 'models/cam572/cam572.png'));
 mapSize = imread(fullfile(CITY_DATA_PATH, 'models/cam572/mapSize.tiff'));
 
+% Takes in the roadMap for a camera and generates the candidates
+cands = Candidates();
+
+% Selective Search wrapper
+%cands = CandidatesSelectSearch('mapSize', mapSize);
+%bboxes = cands.getCandidates('image', image);
+
 % Example boxes
-bboxes = uint32(400 * rand(100, 4) + 1);
+%bboxes = uint32(400 * rand(100, 4) + 1);
 %cands.saveCandidates(bboxes, 'savedBoxes.txt');
 %readBoxes = cands.loadCandidates('savedBoxes.txt');
-debugImg = cands.drawCandidates(bboxes(1:10, :), image);
-figure(3); imshow(debugImg)
+
+% shuffle to output randomly
+bboxes = bboxes(randperm(size(bboxes,1)), :);
+% output by N
+N = 50;
+for i = 0 : floor(size(bboxes,1) / N)
+    subset = bboxes(i*N+1 : min(size(bboxes,1),(i+1)*N), :);
+    if isempty(subset), break; end
+    debugImg = cands.drawCandidates(subset, image);
+    figure(3); imshow(debugImg)
+    waitforbuttonpress();
+end
