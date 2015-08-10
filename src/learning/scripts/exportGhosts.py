220c3dcaa9574ac0ba2f9fb52823597fdd43572e
Evgeny Toropov <etoropov@andrew.cmu.edu>
2015-08-10 16:29:36 -0400

    renamed clustering into exporting

--- src/learning/scripts/exportGhosts.py
+++ src/learning/scripts/exportGhosts.py
@@ -2,7 +2,7 @@ import logging
 import os, sys
 sys.path.insert(0, os.path.abspath('..'))
 from setupHelper import setupLogging
-import clustering
+import exporting
 
 
 setupLogging ('log/learning/clusterGhosts.log', logging.INFO, 'a')
@@ -11,4 +11,4 @@ in_db_path   = 'databases/wr-e0.3.db'
 filters_path = 'clustering/filters/angles.json'
 out_dir      = 'clustering/test'
 
-clustering.collectGhostsTask (in_db_path, filters_path, out_dir)
+exporting.collectGhostsTask (in_db_path, filters_path, out_dir)
