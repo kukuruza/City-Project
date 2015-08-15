814d2236c757d19e5f7ee6c87514a860ba34f1f9
Evgeny Toropov <etoropov@andrew.cmu.edu>
2015-08-15 12:07:19 -0400

    modified example for cnn deployment-python

--- src/learning/dbCnn.py
+++ src/learning/dbCnn.py
@@ -9,12 +9,27 @@ import numpy as np, cv2
 from dbInterface import queryCars, queryField
 from utilities import bbox2roi
 from dbBase import BaseProcessor
-
+sys.path.insert(0, os.path.join(os.getenv('CITY_PATH'), 'src/cnn'))
+from DeploymentPatches import DeploymentPatches
 
 
 class CnnProcessor (BaseProcessor):
 
+    def __init__ (self, db_in_path, db_out_path = None, params = {}):
+        super(CnnProcessor, self).__init__ (db_in_path, db_out_path)
+
+        self.verifyParamThere (params, 'network_path')
+        self.verifyParamThere (params, 'model_path')
+
+        self.deployment = DeploymentPatches(params['network_path'], params['model_path'])
+
+
+
     def importLabels (self, labels_path, params = {}):
+        '''
+        This function is from the times when deployment gave a text file with labels
+        Will be removed later
+        '''
         logging.info ('==== importLabels ====')
         c = self.cursor
 
@@ -45,3 +60,46 @@ class CnnProcessor (BaseProcessor):
         logging.info ('num cars:   ' + str(len(carids)))
     
         return self
+
+
+    def classifyDb (self, params):
+        '''
+        Use CNN python interface to classify .db
+        '''
+        logging.info ('==== dbCnn.classifyDb ====')
+        c = self.cursor
+
+        self.verifyParamThere (params, 'resize')
+        assert (type(params['resize']) == list and len(params['resize']) == 2)
+
+        c.execute('SELECT * FROM cars')
+        car_entries = c.fetchall()
+
+        for car_entry in car_entries:
+
+            # get the ghost
+            imagefile = queryField(car_entry, 'imagefile')
+            c.execute('SELECT ghostfile FROM images WHERE imagefile = ?', (imagefile,))
+            (ghostfile,) = c.fetchone()
+            ghostpath = op.join (self.CITY_DATA_PATH, imagefile)
+            if not op.exists (ghostpath):
+                raise Exception ('ghostfile does not exist: %s' % ghostpath)
+            ghost = cv2.imread(ghostpath)
+
+            # extract patch
+            bbox = queryField(car_entry, 'bbox')
+            roi = bbox2roi(bbox)
+            patch = ghost [roi[0]:roi[2]+1, roi[1]:roi[3]+1]
+            # resize
+            patch = cv2.resize(patch, tuple(params['resize']))
+
+            # work horse
+            label = self.deployment.classify(patch)
+
+            # save label
+            carid = queryField(car_entry, 'id')
+            c.execute('UPDATE cars SET name=? WHERE id=?', (label, carid))
+
+
+        return self
+
