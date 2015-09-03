3acd33f3ae5490cf25bd060ada44ccc3900c8269
satwikkottur <satwik27@gmail.com>
2015-09-03 18:49:58 -0400

    DatabaseWriter accounted for new database structure

--- src/candidates/DatabaseWriterDemo.m
+++ src/candidates/DatabaseWriterDemo.m
@@ -1,7 +1,7 @@
 % Save the candidates into a database using the database writer
 % Demo for DataabaseWriter for all the features
 
-clear all;
+%clear all;
 
 % set paths
 assert (~isempty(getenv('CITY_DATA_PATH')));  % make sure environm. var set
@@ -11,45 +11,51 @@ cd (fileparts(mfilename('fullpath')));        % change dir to this script
 
 % Setting up the paths for input and reference database
 dbPath = fullfile(CITY_DATA_PATH, 'cnn/candidates', '671-Mar24-12h_selective.db');
+%dbPath = fullfile(CITY_DATA_PATH, 'cnn/candidates', '671-Mar24-12h_sizemap.db');
 refPath = fullfile(CITY_DATA_PATH, 'datasets/sparse/Databases/671-Mar24-12h',...
-                    'src.db');
-
-% Creating a simple DatabaseWriter instance
-writer = DatabaseWriter(dbPath, refPath);             
-
-% writing a test bbox for functionality verification
-% writer.saveBoxesToDatabase([1 2 3 4], 'someImage.png');
+                    'src-image.db');
 
 % CameraId
 sizeMapPath = fullfile(CITY_DATA_PATH, 'models/cam671/', 'mapSize.tiff');
-imageDir = fullfile(CITY_DATA_PATH, 'datasets/sparse/Images/671-Mar24-12h/');
+imageSetPath = 'datasets/sparse/Images/671-Mar24-12h';
+imageDir = fullfile(CITY_DATA_PATH, imageSetPath);
+
+% Creating a simple DatabaseWriter instance
+writer = DatabaseWriter(dbPath, refPath, imageSetPath);
 
 % Size map
 mapSize = imread(sizeMapPath);
 % Reading the images in a loop
-imgListing = dir([imageDir, '*.jpg']);
+imgListing = dir(fullfile(imageDir, '*.jpg'));
 
+% Getting the boxes before for the camera(only if based on sizemap
+% candidates)
+%cands = CandidatesSizemap (mapSize);
+%imageBboxes = cands.getCandidates();
+%bg = BackgroundGMM();
+
+% Selective Search candidates
+cands = CandidatesSelectSearch('mapSize', mapSize);
+    
 for i = 1:length(imgListing)
     image = imread(fullfile(imageDir, imgListing(i).name));
     
     % Size map based candidates
-    %cands = CandidatesSizemap (mapSize);
+    % Get the candidates filtered by background
+    %background = bg.subtract(image);
+    %bboxes = cands.filterCandidatesBackground(imageBboxes, background);
     
-    % Selective Search wrapper
-    cands = CandidatesSelectSearch('mapSize', mapSize);
-    
-    % Getting the candidates
+    % Getting the candidates (selective search)
     bboxes = cands.getCandidates('image', image);
     
-%     debugImg = cands.drawCandidates(bboxes, image);
+    %debugImg = cands.drawCandidates(bboxes, image);
 %     bboxes = cands.getCandidates('image', image);
     
     % Writing the candidates
-    writer.saveBoxesToDatabase(bboxes, imgListing(i).name);
+    writer.saveBoxesToDatabase(bboxes, i, size(image));
     
     % Displaying the image
     fprintf('%d / %d\n', i, length(imgListing));
-%     figure(1); imshow(debugImg)
 end
 
 % Closing the database / DatabaseWriter
--- src/io/DatabaseWriter.m
+++ src/io/DatabaseWriter.m
@@ -6,36 +6,57 @@ classdef DatabaseWriter < handle
     properties
         dbPath; % Path of the database
         dbId; % Id for the database session
-        carId;  % Id for car
+        carId = 1;  % Id for car
+        imageId = 1; % Id for image
+        setId; % Id for the set
     end
     
     methods
         % Constructor
         % Takes in a reference path from which the database is copied
         % Also, the output path
-        function self = DatabaseWriter(dbPath, refPath)
+        function self = DatabaseWriter(dbPath, refPath, imageSetPath)
             self.dbPath = dbPath;
             
             % Copying the db path from reference, deleting the tables
             copyfile(refPath, dbPath, 'f'); 
             self.dbId = sqlite3.open (dbPath);
+            
+            % clear the cars table
             sqlite3.execute(self.dbId, 'DELETE FROM cars');
+            
+            % Clear the images table
+            sqlite3.execute(self.dbId, 'DELETE FROM images');
+            
             sqlite3.close(self.dbId);
             
             % Opening the db file for further writing
             self.dbId = sqlite3.open (dbPath);
+            
+            % Search for the set id
+            self.setId = self.getSetId(imageSetPath);
         end
         
         % Write the bounding boxes into the database
-        function saveBoxesToDatabase(self, bboxes, imageName)
+        function saveBoxesToDatabase(self, bboxes, frameId, imSize)
+            % First save the frame
+            query = 'INSERT INTO images(imageid, setid, setentry, width, height) VALUES (?,?,?,?,?)';
+            sqlite3.execute(self.dbId, query, self.imageId, self.setId, frameId, ...
+                                    imSize(1), imSize(2));
+            
+            % Now save the bounding boxes
             for i = 1 : size(bboxes,1)
                 bbox = bboxes(i,:);
-                query = 'INSERT INTO cars(id,imagefile,name,x1,y1,width,height) VALUES (?,?,?,?,?,?,?)';
-                sqlite3.execute(self.dbId, query, self.carId, imageName, 'candidate', ...
+                query = 'INSERT INTO cars(id,imageid,name,x1,y1,width,height) VALUES (?,?,?,?,?,?,?)';
+                sqlite3.execute(self.dbId, query, self.carId, self.imageId, ...
+                                    'candidate', ...
                                     bbox(1), bbox(2), bbox(3), bbox(4));
 
                 self.carId = self.carId + 1;
             end
+            
+            % Increment the image
+            self.imageId = self.imageId + 1;
         end
         
         % Opening the db file
@@ -50,6 +71,13 @@ classdef DatabaseWriter < handle
             if(~isempty(self.dbId))
                 sqlite3.close(self.dbId);
             end
+        end
+        
+        % Finding the set id of the current frame
+        function[setId] = getSetId(self, imageSetPath)
+            query = 'SELECT setid FROM sets WHERE imageset = ?';
+            result = sqlite3.execute(self.dbId, query, imageSetPath);
+            setId = result.setid;
         end
     end
 end
