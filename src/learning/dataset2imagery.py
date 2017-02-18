e2b73ccd19722d50a37d690ffd7f790c1d3285ca
Evgeny Toropov <etoropov@andrew.cmu.edu>
2017-02-18 18:46:30 -0500

    started frontlight

---
+++ src/learning/dataset2imagery.py
@@ -0,0 +1,104 @@
+import os, sys, os.path as op
+import numpy as np
+import cv2
+import logging
+import sqlite3
+import datetime
+from helperSetup import atcity, setParamUnlessThere, assertParamIsThere
+from helperDb    import createDb, imageField
+from helperImg   import ReaderVideo
+
+
+def exportVideo (c, params = {}):
+  ''' Write video with bounding boxes '''
+  
+  logging.info ('==== exportVideo ====')
+  setParamUnlessThere (params, 'relpath', os.getenv('CITY_DATA_PATH'))
+  assertParamIsThere  (params, 'image_processor')
+
+  c.execute('SELECT imagefile FROM images')
+  for (imagefile,) in c.fetchall():
+
+      frame = params['image_processor'].imread(imagefile)
+
+      c.execute('SELECT * FROM cars WHERE imagefile=?', (imagefile,))
+      for car_entry in c.fetchall():
+          roi       = bbox2roi (carField(car_entry, 'bbox'))
+          imagefile = carField (car_entry, 'imagefile')
+          name      = carField (car_entry, 'name')
+          score     = carField (car_entry, 'score')
+
+          if score is None: score = 1
+          logging.debug ('roi: %s, score: %f' % (str(roi), score))
+          drawScoredRoi (frame, roi, name, score)
+
+      params['image_processor'].imwrite(frame, imagefile)
+
+
+
+def dataset2video (c, out_image_video_file=None, out_mask_video_file=None):
+  '''
+  Write each frame from a db to several of files image.avi, mask.avi, time.txt
+  Args:
+    c - cursor
+    image_video_file, mask_video_file, time_file - output file paths 
+        with respect to CITY_DATA_PATH. If None, then not written.
+  '''
+  logging.info ('==== dataset2video ====')
+
+  logging.info ('image file: %s' % out_image_video_file)
+  logging.info ('mask file:  %s' % out_mask_video_file)
+
+  # Assume mask field is null. Deduce the out mask name from imagefile.
+  #   Also send the image video to processor, so that it deduces video params.
+  c.execute('SELECT imagefile,maskfile,width,height FROM images')
+  image_entries = c.fetchall()
+  in_image_video_file = '%s.avi' % op.dirname(image_entries[0][0])
+  in_mask_video_file  = '%s.avi' % op.dirname(image_entries[0][1])
+
+  processor = ProcessorVideo(\
+         ({'out_dataset': {in_image_video_file: out_image_video_file,
+                           in_mask_video_file:  out_mask_video_file} }))
+
+  for i, (imagefile, maskfile, _, _) in enumerate(image_entries):
+    image = processor.imread   (imagefile)
+    mask  = processor.maskread (maskfile)
+    if out_image_video_file is not None: processor.imwrite   (image, imagefile)
+    if out_mask_video_file  is not None: processor.maskwrite (mask, maskfile)
+
+
+
+def dataset2images (c, out_image_dir=None, out_mask_dir=None):
+  ''' Write each frame and mask from a db to its directories '''
+
+  logging.info ('==== dataset2video ====')
+
+  logging.info ('image dir: %s' % out_image_dir)
+  logging.info ('mask dir:  %s' % out_mask_dir)
+
+  # create directories
+  if out_image_dir is not None:
+    if not op.exists (atcity(out_image_dir)):
+      os.makedirs (atcity(out_image_dir))
+  if out_mask_dir is not None:
+    if not op.exists (atcity(out_mask_dir)):
+      os.makedirs (atcity(out_mask_dir))
+
+  # Assume mask field is null. Deduce the out mask name from imagefile.
+  #   Also send the image video to processor, so that it deduces video params.
+  c.execute('SELECT imagefile,maskfile FROM images')
+  image_entries = c.fetchall()
+
+  reader = ReaderVideo()
+
+  for imagefile, maskfile in image_entries:
+    if out_image_dir is not None:
+      image = reader.imread   (imagefile)
+      imagepath = atcity(op.join(out_image_dir, '%s.jpg' % op.basename(imagefile)))
+      logging.debug ('will write imagefile to %s' % imagepath)
+      result = cv2.imwrite(imagepath, image)
+    if out_mask_dir  is not None: 
+      mask  = reader.maskread (maskfile)
+      maskpath  = atcity(op.join(out_mask_dir, '%s.png' % op.basename(maskfile)))
+      logging.debug ('will write maskpath to %s' % maskpath)
+      cv2.imwrite(maskpath, mask)
