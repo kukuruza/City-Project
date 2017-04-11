0e1630106b61fbe1022c6acdf179123eda2914a1
Evgeny Toropov <etoropov@andrew.cmu.edu>
2017-04-11 16:28:19 -0400

    server has a single function detect()

--- src/server/viewerServer/viewerServer/urls.py
+++ src/server/viewerServer/viewerServer/urls.py
@@ -15,7 +15,15 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from viewerServer.views import detect
 
 urlpatterns = [
+    url(r'^(\d+)/$', detect),
     url(r'^admin/', admin.site.urls),
 ]
+
+from django.conf import settings
+from django.conf.urls.static import  static
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
