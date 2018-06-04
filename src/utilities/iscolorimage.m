1ea430cb4756c8965d6ebca2af8cea11219c3917
Evgeny Toropov <etoropov@andrew.cmu.edu>
2018-06-04 10:45:41 -0400

    removed unnecessary dirs in src

--- src/utilities/iscolorimage.m
+++
@@ -1,5 +0,0 @@
-function check = iscolorimage (A)
-% checks if the input is a color image
-%   function used in inputParser to save space
-
-check = ndims(A) == 3 && size(A,3) == 3 && isa(A, 'uint8');
--- src/utilities/matlabDatetime2dbTime.m
+++
@@ -1,9 +0,0 @@
-function s = matlabDatetime2dbTime (t)
-% change matlab datetime format to our 'YYYY-MM-DD HH:MM:SS.mic'
-
-    parser = inputParser;
-    addRequired (parser, 't', @(x) isa(x, 'datetime'));
-    parse (parser, t);
-
-    s = datestr(t, 'yyyy-mm-dd HH:MM:SS.FFF');
-end
